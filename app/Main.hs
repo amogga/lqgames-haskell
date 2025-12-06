69cbcef4afd282f90b9c811ecf9a99439e6a5f6a
Ahmed Mogahed <a.mogahed@inginne.com>
2025-12-06 20:39:25 +0200

    Update main

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
 module Main (main) where
 
+import Numeric.LinearAlgebra
+
 main :: IO ()
 main = do
-    -- let states = vector [6.5, 0.0, pi / 2.0 , 1.0, 1.5, 40, -(pi / 2.0), 0.1, 0.0, 22.0, 0.0, 2.0]
-    -- let input = vector [0,0,0,0,0,0]
-    let states = [6.5, 0.0, pi / 2.0 , 1.0, 1.5, 40, -(pi / 2.0), 0.1, 0.0, 22.0, 0.0, 2.0]
-    let input = [0,0,0,0,0,0]
-
-    putStrLn "Test"
+    let states = vector [6.5, 0.0, pi / 2.0 , 1.0, 1.5, 40, -(pi / 2.0), 0.1, 0.0, 22.0, 0.0, 2.0]
+        input = vector [0,0,0,0,0,0]
+    
+    print (states, input)
