ebc22437028524c4a4cbf221738dd21bb1512316
Ahmed Mogahed <a.mogahed@inginne.com>
2025-12-07 23:24:22 +0200

    Cleanup main for example 5

--- examples/Example5/Main.hs
+++ examples/Example5/Main.hs
@@ -6,7 +6,6 @@ import Type.Simulation
 main :: IO ()
 main = do
     -- run simulation iterations
-    -- let simParamsMax = SimulationParametersWithMaxTime {iterationCount = 60, sample = 0.25, maxTime = 8}
     let iters = runSimulationWithIterationAndHorizonE simParamsHorizon initState initInput
 
     -- compute costs per iteration
